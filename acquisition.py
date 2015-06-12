30156f1e7d533c81b17ccf477053930a40704542
Jonathan Sieber <jonathan_sieber@yahoo.de>
2015-06-12 17:08:55 +0200

    Integrate the OpenBCI driver in liveplot

---
+++ acquisition.py
@@ -0,0 +1,47 @@
+from PySide import QtCore, QtGui
+from open_bci_v3 import OpenBCIBoard
+from bdf import BDFReader
+
+class OpenBCIThread(QtCore.QThread):
+  newPacket = QtCore.Signal(object)
+  
+  def __init__(self, ttyPath = '/dev/ttyUSB0'):
+    super(OpenBCIThread, self).__init__()
+    
+    self.board = OpenBCIBoard(ttyPath, scaled_output=False)
+
+  def handlePacket(self, sample):
+    self.newPacket.emit(sample.channel_data)
+    QtGui.QApplication.instance().processEvents()
+    
+  def run(self):
+    try:
+      self.board.print_register_settings()
+      self.board.start_streaming(self.handlePacket)
+
+    # Important workaround for OSX: If the input is not kept on being read, the whole (input sub-)system freezes
+    except Exception, e:
+      print "Exception", e
+      while True:
+        self.board.ser.read()
+    
+class BDFThread(QtCore.QThread):
+  newPacket = QtCore.Signal(object)
+
+  def __init__(self, filename):
+    super(BDFThread, self).__init__()
+    self.bdf = BDFReader(file(filename, 'rb'))
+
+  def emitPacket(self):
+    packet =  self.bdf.readPacket()
+    if packet:
+        self.newPacket.emit(packet)
+    else:
+        self.quit()
+
+  def run(self):
+    timer = QtCore.QTimer()
+    timer.timeout.connect(self.emitPacket)
+    timer.start(1000 / 250)
+
+    self.exec_()
--- liveplot.py
+++ liveplot.py
@@ -4,19 +4,20 @@ import pyqtgraph as pg
 import scipy.signal
 
 from PySide import QtCore
-from BDFReader import BDFReader
-from SpectrogramWidget import SpectrogramWidget
+from bdf import BDFReader, BDFWriter
+from acquisition import BDFThread, OpenBCIThread
 
+from SpectrogramWidget import SpectrogramWidget
 import pyeeg
 import sys
 
-# QtGui.QApplication.setGraphicsSystem('raster')
+#QtGui.QApplication.setGraphicsSystem('raster')
 app = QtGui.QApplication([])
-# mw = QtGui.QMainWindow()
-# mw.resize(800,800)
+#mw = QtGui.QMainWindow()
+#mw.resize(800,800)
 
 win = pg.GraphicsWindow(title="Basic plotting examples")
-# win.resize(1000,600)
+#win.resize(1000,600)
 win.setWindowTitle('pyqtgraph example: Plotting')
 
 # Enable antialiasing for prettier plots
@@ -32,12 +33,38 @@ p6.setXRange(0, 500)
 p6.enableAutoRange('x', True)
 p6.enableAutoRange('y', 0.8)
 
+class ViewBoxEndRanger:
+  def __init__(self, viewBox):
+    self.viewBox = viewBox
+    viewBox.sigRangeChanged.connect(self.rangeChanged)
+    viewBox.sigRangeChangedManually.connect(self.rangeChangedManually)
+    viewBox.childrenChanged.connect(self.rangeChanged2)
+    (xmin, xmax), _ = viewBox.viewRange()
+    self.xwidth = xmax - xmin
+
+  def rangeChanged2(self, foo):
+    print "children changed", foo
+
+  def rangeChanged(self, foo):
+    (xmin, xmax), _ = self.viewBox.viewRange()
+    xwidth = xmax-xmin
+    (_, xmax), _ = self.viewBox.childrenBounds()
+    p6.setRange(xRange=(xmax- xwidth*0.9, xmax + xwidth * 0.1), padding=0, update=False, disableAutoRange=False)
+
+  def rangeChangedManually(self, _):
+    self.viewBox.enableAutoRange('x', True)
+    self.viewBox.enableAutoRange('y', 0.8)
+
+vber = ViewBoxEndRanger(p6.getViewBox())
+#p6.sigRangeChanged.connect(updateRange)
+#p6.getViewBox().sigRangeChangedManually.connect(updateRangeManually)
+
+
 firHighPass = scipy.signal.firwin(199, 4.0, pass_zero=False, nyq=125.0)
 firBandPass = scipy.signal.firwin(199, (4.0, 35.0), pass_zero=False, nyq=125.0)
 iirBandPass = scipy.signal.iirfilter(17, [4.0 / 125., 35.0 / 125.], btype='band')
 
 filter = firBandPass
-
 
 @QtCore.Slot(object)
 def handlePacket(packet):
@@ -57,38 +84,27 @@ def handlePacket(packet):
     data2 = np.append(data2, newValue)
 
 
-    # curve.setData(data)
+    #curve.setData(data)
     curve2.setData(data2)
 
     power, ratios = pyeeg.bin_power(data2[-250:], [4, 8, 12, 15, 20, 30, 50], 250)
     foo.setOpts(height=ratios)
 
+sourceThread = BDFThread(sys.argv[1])
+#sourceThread = OpenBCIThread(sys.argv[1])
 
-class BDFThread(QtCore.QThread):
-    newPacket = QtCore.Signal(object)
+try:
+  write_file = file(sys.argv[2], 'wb')
+  import bdf
+  writer = bdf.BDF(8)
+  sourceThread.newPacket.connect(writer.append_sample)
+  QtGui.QApplication.instance().aboutToQuit.connect(lambda: writer.write_file(write_file))
+except IndexError:
+  print "No log file specified"
+  pass
 
-    def __init__(self, filename):
-        super(BDFThread, self).__init__()
-        self.bdf = BDFReader(file(filename, 'rb'))
-
-    def emitPacket(self):
-        packet = self.bdf.readPacket()
-        if packet:
-            self.newPacket.emit(packet)
-        else:
-            self.quit()
-
-    def run(self):
-        timer = QtCore.QTimer()
-        timer.timeout.connect(self.emitPacket)
-        timer.start(1000 / 250)
-
-        self.exec_()
-
-
-bdf = BDFThread(sys.argv[1])
-bdf.newPacket.connect(handlePacket)
-bdf.start()
+sourceThread.newPacket.connect(handlePacket)
+sourceThread.start()
 
 win.nextRow()
 p7 = win.addPlot(title='Frequency distribution')
@@ -97,10 +113,11 @@ p7.addItem(foo)
 p7.getAxis('bottom').setTicks([zip(range(6), ['Theta', 'Alpha', 'SMR', 'Beta', 'Hi Beta', 'Gamma'])])
 foo.setOpts(brushes=[pg.hsvColor(x / 6.0) for x in range(6)])
 
+
 win.nextRow()
 
-#specPlot = win.addPlot(title='Spectrogram')
-specWidget = SpectrogramWidget()
+##specPlot = win.addPlot(title='Spectrogram')
+#specWidget = SpectrogramWidget()
 #win.addItem(specWidget)
 #specPlot.addItem(specWidget)
 
@@ -108,9 +125,11 @@ def specgramHandler(packet):
     global specWidget
     specWidget.update(packet[0])
 
-bdf.newPacket.connect(specgramHandler)
+#sourceThread.newPacket.connect(specgramHandler)
+
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+       QtGui.QApplication.instance().exec_()
+
